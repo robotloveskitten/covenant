1253b19899691ddf43585114d73d3f039298cfd3
Tim Barkow <tim@thinkcorps.com>
2026-01-08 22:47:24 -0600

    assignee styling

--- app/javascript/controllers/combobox_controller.js
+++ app/javascript/controllers/combobox_controller.js
@@ -119,17 +119,17 @@ export default class extends Controller {
   updateSelectedDisplay(name) {
     if (name) {
       this.selectedTarget.innerHTML = `
-        <div class="flex items-center gap-2">
-          <div class="w-6 h-6 rounded-full bg-base-300 flex items-center justify-center text-xs font-medium">
+        <span class="inline-flex items-center gap-2" style="white-space: nowrap;">
+          <span class="inline-flex w-6 h-6 rounded-full bg-base-300 items-center justify-center text-xs font-medium flex-none">
             ${name.charAt(0).toUpperCase()}
-          </div>
+          </span>
           <span class="text-sm">${this.escapeHtml(name)}</span>
-          <button type="button" data-action="click->combobox#clear" class="ml-auto text-base-content/40 hover:text-base-content">
+          <button type="button" data-action="click->combobox#clear" class="inline-flex flex-none text-base-content/40 hover:text-base-content">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
               <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
             </svg>
           </button>
-        </div>
+        </span>
       `
     } else {
       this.selectedTarget.innerHTML = `
